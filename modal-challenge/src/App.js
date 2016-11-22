e389198780ad749f3c787e1f4507dc1208d15e3a
John Colella <colella.john@gmail.com>
2016-11-22 11:25:22 -0500

    review modal and update German beer fact

--- modal-challenge/src/App.js
+++ modal-challenge/src/App.js
@@ -33,13 +33,15 @@ class App extends Component {
           <h2>Quick Beer Facts</h2>
           <p>Click a button to learn a quick fact about the topic</p>
         </div>
+
         <button
           onClick={
-            () => this.handleClickButton( "In 1516, the Reinheitsgebot (German Beer Purity Law) was put in to effect. This law brought organization to brewing, only allowing malt, hops and water to be included in beer. Yeast, which at the time was not discovered, would later be added to the law." )
+            () => this.handleClickButton( "In 1516, the Reinheitsgebot (German Beer Purity Law) was put in to effect. This law helped to standardize brewing in Germany, only allowing malt, hops and water to be included in beer. Yeast, which at the time was not discovered, would later be added to the law." )
           }
         >
           German Beer Law
         </button>
+        
         <button
           onClick={
             () => this.handleClickButton( "Lambic is a sour style that is ingrained in Belgian beer history. To be truly lambic, the beer must be brewed in the traditional Payottenland region in Belgium and make use of wild-fermentation techniques. As it has been done since the brew's inception, lambic is fermented strictly with airbone microbes in the region, truly giving this style its own sense of terrior." )
@@ -47,11 +49,13 @@ class App extends Component {
         >
           Belgian Beer
         </button>
+
         <div className={ (this.state.modal ? "modal-show " : "modal-hide ") + "modal-container" }>
           <ModalContent
             message={ this.state.message }
             onCloseModal={ this.handleCloseModal } />
         </div>
+
       </div>
     );
   }
